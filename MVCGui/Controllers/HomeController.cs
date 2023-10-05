5df4f21f0969b63490eaef95c9c2cc31f6994f87
Peter Thinggaard <pete9817@gmail.com>
2023-10-05 12:40:14 +0200

    fault isolated add

--- MVCGui/Controllers/HomeController.cs
+++ MVCGui/Controllers/HomeController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using MVCGui.Models;
 using System.Diagnostics;
+using Polly;
 
 namespace MVCGui.Controllers
 {
@@ -25,21 +26,50 @@ namespace MVCGui.Controllers
         [HttpPost]
         public IActionResult Add(int leftNumber, int rightNumber)
         {
-            var client = new HttpClient();
-            client.BaseAddress = new Uri("http://additionService/Addition");
+            //fault isolation
+            var retryPolicy = Policy.Handle<HttpRequestException>()
+                .Retry(3, (exception, retryCount) =>
+                {
+                    Console.WriteLine("Retrying because MVCGui ran into" + exception.GetType().Name);
+                });
 
-            var response = client.PostAsync(client.BaseAddress + "?leftNumber=" + leftNumber + "&rightNumber=" + rightNumber, null);
-            Console.WriteLine("Added " + leftNumber + " + " + rightNumber);
-            string result = response.Result.Content.ReadAsStringAsync().Result;
-            Console.WriteLine(result + "RESULTED!!");
+            var fallbackPolicy = Policy.Handle<HttpRequestException>()
+                .Fallback(() =>
+                 {
+                     //if services cannot be reached, we complete the action ourselves.
+                     ViewBag.calculationList = leftNumber + rightNumber;
+                    
+                 });
+            var policy = retryPolicy.Wrap(fallbackPolicy);
 
-
-            List<string> stringList = result.Split(',').ToList();
-            for (int i= 0; i < stringList.Count; i++)
+            policy.Execute(() =>
             {
-                stringList[i] = stringList[i].Trim().Trim('[', ']').Replace("\"", "");
+                var client = new HttpClient();
+                client.BaseAddress = new Uri("http://additionService/Addition");
+
+                var response = client.PostAsync(client.BaseAddress + "?leftNumber=" + leftNumber + "&rightNumber=" + rightNumber, null);
+                Console.WriteLine("Added " + leftNumber + " + " + rightNumber);
+                string result = response.Result.Content.ReadAsStringAsync().Result;
+                Console.WriteLine(result + "RESULTED!!");
+
+
+
+
+             if (!string.IsNullOrEmpty(result))
+                {
+                    List<string> stringList = result.Split(',').ToList();
+                    for (int i = 0; i < stringList.Count; i++)
+                    {
+                        stringList[i] = stringList[i].Trim().Trim('[', ']').Replace("\"", "");
+                    }
+                    ViewBag.calculationList = stringList;
+                }
+            });
+            
+            if (ViewBag.calculationList == null)
+            {
+                ViewBag.CalculationList = new List<String>{"something went wrong"};
             }
-            ViewBag.calculationList = stringList;
             return View("Index");
         }
 
@@ -53,6 +83,14 @@ namespace MVCGui.Controllers
             Console.WriteLine("Subtracted " + leftNumber + " - " + rightNumber);
             String result = response.Result.Content.ReadAsStringAsync().Result;
             Console.WriteLine(result + "subtraction RESULTED!!");
+
+            //fault isolation
+            if (string.IsNullOrEmpty(result))
+            {
+                ViewBag.calculationList = leftNumber - rightNumber;
+                return View("Index");
+            }
+
             List<string> stringList = result.Split(',').ToList();
             for (int i = 0; i < stringList.Count; i++)
             {
--- oblAdditionService/Controllers/AdditionController.cs
+++ oblAdditionService/Controllers/AdditionController.cs
@@ -1,4 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
+using MySql.Data.MySqlClient;
+using Polly;
 //using MySql.Data.MySqlClient;
 using System.Data;
 using static System.Runtime.InteropServices.JavaScript.JSType;
@@ -17,14 +19,28 @@ namespace oblAdditionService.Controllers
         [HttpPost]
         public string Add(int leftNumber, int rightNumber)
         {
-            Console.WriteLine("Got a request for :" + leftNumber);
+            string result = leftNumber + " + " + rightNumber + " = " + (leftNumber + rightNumber);
+            var fallbackPolicy = Policy.Handle<Exception>()
+                .Fallback(() =>
+                 {
+                     //if database cannot be reached, just return the numbers added up.
+                     Console.WriteLine("Fell back, returning singular addition");
 
-            var client = new HttpClient();
-            client.BaseAddress = new Uri("http://historyService/History");
+                 });
 
-            var response = client.PostAsync(client.BaseAddress + "?leftNumber=" + leftNumber + "&rightNumber=" + rightNumber + "&isAddition=" + true + "&result=" + (leftNumber + rightNumber), null);
-            Console.WriteLine("Added " + leftNumber + " + " + rightNumber);
-            string result = response.Result.Content.ReadAsStringAsync().Result;
+            fallbackPolicy.Execute(() =>
+            {
+                Console.WriteLine("Got a request for :" + leftNumber);
+
+                var client = new HttpClient();
+                client.BaseAddress = new Uri("http://historyService/History");
+
+                var response = client.PostAsync(client.BaseAddress + "?leftNumber=" + leftNumber + "&rightNumber=" + rightNumber + "&isAddition=" + true + "&result=" + (leftNumber + rightNumber), null);
+                Console.WriteLine("Added " + leftNumber + " + " + rightNumber);
+                result = response.Result.Content.ReadAsStringAsync().Result;
+            });
+               
+           
 
             return result;
         }
