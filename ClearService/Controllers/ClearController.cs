e5580a452c770de190ea519b9a620ddfdc2b058c
PeterThi <pete9817@gmail.com>
2023-11-17 15:43:55 +0100

    Update ClearController.cs

--- ClearService/Controllers/ClearController.cs
+++ ClearService/Controllers/ClearController.cs
@@ -1,4 +1,8 @@
 using Microsoft.AspNetCore.Mvc;
+using Monitoring;
+using OpenTelemetry;
+using OpenTelemetry.Context.Propagation;
+using System.Diagnostics;
 
 namespace ClearService.Controllers
 {
@@ -15,23 +19,30 @@ namespace ClearService.Controllers
         [HttpGet]
         public String Get()
         {
-            var client = new HttpClient();
-            client.BaseAddress = new Uri("http://historyService/History");
-
-            var request = new HttpRequestMessage(HttpMethod.Put, client.BaseAddress);
-            var response = client.SendAsync(request);
-
-            //Distributed tracing starts.
-            var propagationContext = new PropagationContext(ActivityContext, Baggage.Current);
-            var propagator = new TraceContextPropagator();
-            propagator.Inject(propagationContext, request, (r, Key, value) =>
+            using (var activity = MonitoringService.ActivitySource.StartActivity())
             {
-                r.Headers.Add(Key, value);
-            });
+                var client = new HttpClient();
+                client.BaseAddress = new Uri("http://historyService/History");
+
+                var request = new HttpRequestMessage(HttpMethod.Put, client.BaseAddress);
+                var response = client.SendAsync(request);
+
+                //Distributed tracing starts.
+                var ActivityContext = activity?.Context ?? Activity.Current?.Context ?? default;
+                var propagationContext = new PropagationContext(ActivityContext, Baggage.Current);
+                var propagator = new TraceContextPropagator();
+                propagator.Inject(propagationContext, request, (r, Key, value) =>
+                {
+                    r.Headers.Add(Key, value);
+                });
+
+                var result = response.Result.Content.ReadAsStringAsync().Result;
+                return "Calculator was cleared (ish)" + result;
+            }
 
 
-            var result = response.Result.Content.ReadAsStringAsync().Result;
-            return "Calculator was cleared (ish)" + result;
+
+
         }
 
     }
